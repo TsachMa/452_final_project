71d9a569530f89a1b6fc4f9d16d4acea1fb1a56e
TsachMa <tsach276@mit.edu>
2024-04-23 20:57:30 -0400

    minor changes

--- train_launcher.sh
+++ train_launcher.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+<<<<<<< HEAD
 #SBATCH --partition=education_gpu
 #SBATCH --gpus 1
 #SBATCH --time=12:00:00
 #SBATCH --mem=10000
+=======
+#SBATCH --gpus=p100:1
+#SBATCH --partition=education_gpu
+#SBATCH --time=10:00
+>>>>>>> 6f68613 (minor changes)
 #SBATCH --output="output/%j_output.txt"
 #SBATCH --error="error/%j_error.txt"
 
@@ -14,7 +20,10 @@
 # module load nccl/2.18.1-cuda11.8
 # source /state/partition1/llgrid/pkg/anaconda/anaconda3-2023a/etc/profile.d/conda.sh
 module load miniconda 
+<<<<<<< HEAD
 module load cuDNN/8.7.0.84-CUDA-11.8.0
+=======
+>>>>>>> 6f68613 (minor changes)
 conda activate cpsc552v2
 
 # Pass the max_num_atoms parameter to the Hydra configuration
